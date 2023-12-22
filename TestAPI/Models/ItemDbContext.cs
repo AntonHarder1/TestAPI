f65136385925c01eaf6acbea1707b1ab7df4650c
Kristian-j-k <89967661+Kristian-j-k@users.noreply.github.com>
2023-12-22 23:48:17 +0100

    noget

--- TestAPI/Models/ItemDbContext.cs
+++ TestAPI/Models/ItemDbContext.cs
@@ -7,7 +7,7 @@ namespace TestAPI.Models
     public class ItemDbContext : DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-        {
+        { 
             //options.UseSqlServer(@"Server=tcp:sweden.database.windows.net,1433;Initial Catalog=Table_1;Database=test_1;User ID=test_db_gæs;Password=12ahkk##;Trusted_Connection=False;Encrypt=True");
             options.UseSqlServer(@"Server = tcp:sweden.database.windows.net, 1433; Initial Catalog = test_1; Persist Security Info = False; User ID = test_db_gæs; Password = 12ahkk##; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30; ");
 
--- TestAPI/Program.cs
+++ TestAPI/Program.cs
@@ -26,39 +26,39 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<ItemDbContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen(options =>
-//{
-//    options.SwaggerDoc("v1", new OpenApiInfo
-//    {
-//        Version = "v1",
-//        Title = "ToDo API",
-//        Description = "An ASP.NET Core Web API for managing ToDo items",
-//        TermsOfService = new Uri("https://example.com/terms"),
-//        Contact = new OpenApiContact
-//        {
-//            Name = "Example Contact",
-//            Url = new Uri("https://example.com/contact")
-//        },
-//        License = new OpenApiLicense
-//        {
-//            Name = "Example License",
-//            Url = new Uri("https://example.com/license")
-//        }
-//    });
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "ToDo API",
+        Description = "An ASP.NET Core Web API for managing ToDo items",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
 
-//    // using System.Reflection;
-//    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-//    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-//});
+    // using System.Reflection;
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 var app = builder.Build();
 
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())  // || app.Environment.IsProduction())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())  // || app.Environment.IsProduction())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 
 
